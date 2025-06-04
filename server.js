77aeb2796b533a9dc3131583289caa9812b712a8
Felipe Sakai <dev8@aproms.com.br>
2025-06-04 07:26:27 -0400

    fix(server.js): corrigindo para funcionar no render

--- server.js
+++ server.js
@@ -181,5 +181,5 @@ function broadcast(message, exclude) {
   });
 }
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Servidor rodando em http://localhost:${PORT}`));
